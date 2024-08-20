a45d3a7594f3b841595a10cfe196dfd6fb39bb45
LinkPovilas <linkpovilas@gmail.com>
2024-08-20 12:36:17 +0300

    chore: change deps and refactor

--- env.ts
+++ env.ts
@@ -1,9 +1,11 @@
 import 'dotenv/config';
-import { cleanEnv, bool, url } from 'envalid';
+import { z } from 'zod';
 
-const env = cleanEnv(process.env, {
-  CI: bool({ default: false }),
-  BASE_URL: url()
+const envSchema = z.object({
+  CI: z.union([z.string(), z.boolean()]).default(false),
+  BASE_URL: z.string().url()
 });
+
+const env = envSchema.parse(process.env);
 
 export default env;
